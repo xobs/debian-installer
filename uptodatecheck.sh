0a3de31e311f289a484b2365f76deb4197acd2c9
Petter Reinholdtsen <pere@debian.org>
2002-09-21 09:28:08 +0000

    Exit if madison is missing.
    r1002

--- uptodatecheck.sh
+++ uptodatecheck.sh
@@ -2,6 +2,14 @@
 
 # Check up-to-dateness of udebs in the archive
 
+# First check if madison is available.
+if madison ash > /dev/null 2>&1 ; then
+	:
+else
+	echo "Error: Unable to find the madison program.  Exiting."
+	exit 1
+fi
+
 PACKAGES="anna main-menu retriever/cdrom retriever/choose-mirror retriever/file retriever/wget rootskel libdebian-installer tools/autopartkit tools/ddetect tools/base-installer tools/grub-installer tools/kdetect tools/lilo-installer tools/lmod-detect-pci tools/cdebconf tools/netcfg tools/partkit tools/prebaseconfig tools/cdrom-detect tools/selectdevice tools/udpkg utils"
 
 cd ..
