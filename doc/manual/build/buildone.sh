c0bfef1113b52700c71f3d2728235da5abcf44d3
Frans Pop <fjp@debian.org>
2004-09-16 18:35:54 +0000

    Manual: Reverting some changes that should have stayed local :-(
    r21779

--- doc/manual/build/buildone.sh
+++ doc/manual/build/buildone.sh
@@ -18,80 +18,82 @@ checkresult () {
 ## First we define some paths to various xsl stylesheets
 stylesheet_profile="/usr/share/sgml/docbook/stylesheet/xsl/nwalsh/profiling/profile.xsl"
 stylesheet_html="style-html.xsl"
+stylesheet_fo="style-fo.xsl"
 
 ## Location to our tools
 xsltprocessor=xsltproc
+foprocessor=./fop/fop.sh
 
 ## Build preparation
 dynamic="dynamic.ent"
 
 # Official/unofficial builds.
 if [ ! "$official_build" ]; then
-	unofficial_build="FIXME;unofficial-build"
+        unofficial_build="FIXME;unofficial-build"
 else
-	unofficial_build=""
+        unofficial_build=""
 fi
 
 ## Now we have to setup correct profiling information for each architecture
 case $arch in
     alpha)
         archspec="alpha;not-i386;not-s390;not-m68k;not-powerpc"
-	kernelversion="2.4.27"
-	
+        kernelversion="2.4.27"
+
         fdisk="fdisk.txt;cfdisk.txt"
         network="supports-tftp;supports-nfsroot"
         boot="supports-floppy-boot"
         other="supports-serial-console;rescue-needs-root-disk"
         smp="supports-smp"
         goodies=""
-	;;
+        ;;
     arm)
         archspec="arm;not-i386;not-s390;not-m68k;not-powerpc;not-alpha"
-	kernelversion="2.4.27"
-		
+        kernelversion="2.4.27"
+
         fdisk="fdisk.txt;cfdisk.txt"
         network="supports-tftp;supports-rarp;supports-dhcp;supports-bootp;supports-nfsroot"
         boot=""
         other="supports-serial-console;rescue-needs-root-disk"
         smp=""
         goodies="supports-lang-chooser"
-	;;
+        ;;
     hppa)
         archspec="hppa;not-i386;not-s390;not-m68k;not-powerpc;not-alpha"
-	kernelversion="2.4.27"
-	
+        kernelversion="2.4.27"
+
         fdisk="cfdisk.txt"
         network="supports-tftp;supports-dhcp;supports-bootp;supports-nfsroot"
         boot=""
         other="supports-serial-console;rescue-needs-root-disk"
         smp=""
         goodies="supports-lang-chooser"
-	;;
+        ;;
     i386)
         archspec="i386;not-s390;not-m68k;not-powerpc;not-alpha"
-	kernelversion="2.4.27"
-	
+        kernelversion="2.4.27"
+
         fdisk="fdisk.txt;cfdisk.txt"
         network="supports-tftp;supports-dhcp;supports-bootp;supports-nfsroot"
         boot="supports-floppy-boot;bootable-disk;bootable-usb"
         other="supports-pcmcia;supports-serial-console;rescue-needs-root-disk;workaround-bug-99926"
         smp="supports-smp-sometimes"
         goodies="supports-lang-chooser"
-	;;
+        ;;
     ia64)
         archspec="ia64;not-i386;not-s390;not-m68k;not-powerpc;not-alpha"
-	kernelversion="2.4.27"
-	
+        kernelversion="2.4.27"
+
         fdisk="parted.txt;cfdisk.txt"
         network="supports-tftp;supports-rarp;supports-dhcp;supports-bootp;supports-nfsroot"
         boot=""
         other="supports-serial-console;rescue-needs-root-disk"
         smp="supports-smp"
         goodies="supports-lang-chooser"
-	;;
+        ;;
     m68k)
         archspec="m68k;not-i386;not-s390;not-powerpc;not-alpha"
-	kernelversion="2.4.27"
+        kernelversion="2.4.27"
 
         fdisk="atari-fdisk.txt;mac-fdisk.txt;amiga-fdisk.txt;pmac-fdisk.txt"
         network="supports-tftp;supports-rarp;supports-dhcp;supports-bootp;supports-nfsroot"
@@ -99,65 +101,65 @@ case $arch in
         other="supports-serial-console;rescue-needs-root-disk"
         smp=""
         goodies="supports-lang-chooser"
-	;;
+        ;;
     mips)
         archspec="mips;not-i386;not-s390;not-m68k;not-powerpc;not-alpha"
-	kernelversion="2.4.27"
-	
+        kernelversion="2.4.27"
+
         fdisk="fdisk.txt;cfdisk.txt"
         network="supports-tftp;supports-nfsroot"
         boot=""
         other="supports-serial-console;rescue-needs-root-disk"
         smp=""
         goodies=""
-	;;
+        ;;
     mipsel)
         archspec="mipsel;not-i386;not-s390;not-m68k;not-powerpc;not-alpha"
-	kernelversion="2.4.27"
-	
+        kernelversion="2.4.27"
+
         fdisk="fdisk.txt;cfdisk.txt"
         network="supports-tftp;supports-dhcp;supports-bootp;supports-nfsroot"
         boot=""
         other="supports-serial-console;rescue-needs-root-disk"
         smp=""
         goodies=""
-	;;
+        ;;
     powerpc)
         archspec="powerpc;not-s390;not-m68k;not-i386;not-alpha"
-	kernelversion="2.6.8"
-	
+        kernelversion="2.6.8"
+
         fdisk="mac-fdisk.txt;cfdisk.txt"
         network="supports-tftp;supports-dhcp;supports-bootp;supports-nfsroot"
         boot="supports-floppy-boot;bootable-disk"
         other="supports-pcmcia;supports-serial-console;rescue-needs-root-disk"
         smp="supports-smp"
         goodies="supports-lang-chooser"
-	;;
+        ;;
     s390)
         archspec="s390;not-powerpc;not-m68k;not-i386;not-alpha"
-	kernelversion="2.4.27"
-	
+        kernelversion="2.4.27"
+        
         fdisk="fdasd.txt;dasdfmt.txt"
         network="supports-nfsroot"
         boot=""
         other="rescue-needs-root-disk"
         smp="defaults-smp"
         goodies=""
-	;;
+        ;;
     sparc)
         archspec="sparc;not-i386;not-s390;not-m68k;not-powerpc;not-alpha"
-	kernelversion="2.4.27"
-	
+        kernelversion="2.4.27"
+
         fdisk="fdisk.txt"
         network="supports-tftp;supports-rarp;supports-dhcp;supports-bootp;supports-nfsroot"
         boot="supports-floppy-boot"
         other="supports-serial-console;rescue-needs-root-disk"
         smp="supports-smp"
         goodies=""
-	;;
+        ;;
     *)
         echo "Unknown architecture ${arch}! Please elaborate."
-	exit 1 ;;
+        exit 1 ;;
 esac
 
 ## Join all gathered info into one big variable
@@ -186,6 +188,12 @@ checkresult $?
 $xsltprocessor $stylesheet_html install.${language}.profiled.xml
 checkresult $?
 
-## ...and optionally we can generate (little bit ugly) pdf/ps/txt.
-# ./buildfop.sh pdf ${language}
+## ...and also to the .fo...
+# $xsltprocessor --output install.${language}.fo \
+#                $stylesheet_fo install.${language}.profiled.xml
 # checkresult $?
+
+## ...from which we can generate (little bit ugly) pdf/ps/txt.
+# $foprocessor -fo install.${language}.fo -pdf install.${language}.pdf
+# checkresult $?
+
