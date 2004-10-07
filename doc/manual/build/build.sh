3129df7f31a91638232e352fe7e6b7bad828ea85
Frans Pop <fjp@debian.org>
2004-10-07 17:32:56 +0000

    Setting languages to be build for RC2 release
    r22811

--- doc/manual/build/build.sh
+++ doc/manual/build/build.sh
@@ -2,7 +2,10 @@
 
 if [ -z "$languages" ]; then
 	# Please add languages only if they build properly.
-	languages="en cs es fr ja nl pt_BR" # ca da de el eu it ru
+	# languages="en cs es fr ja nl pt_BR" # ca da de el eu it ru
+
+	# Buildlist of languages to be included on RC2 CD's
+	languages="en cs fr ja"
 fi
 
 if [ -z "$architectures" ]; then
