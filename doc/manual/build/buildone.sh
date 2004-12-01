04ad504617823c34a2ab5201087d1fc1731bc7c3
Frans Pop <fjp@debian.org>
2004-12-01 18:15:32 +0000

    Switch to new-generation build scripts for d-i manual: - move new scripts from new-generation to build
    r24102

--- doc/manual/build/buildone.sh
+++ doc/manual/build/buildone.sh
@@ -13,8 +13,8 @@ formats=${3:-html}
 
 ## Configuration
 basedir="$(cd "$(dirname $0)"; pwd)"
-manual_path="$(echo $basedir | sed "s:/build/new-generation$::")"
-build_path="$manual_path/build/new-generation"
+manual_path="$(echo $basedir | sed "s:/build$::")"
+build_path="$manual_path/build"
 cd $build_path
 
 stylesheet_dir="$build_path/stylesheets"
