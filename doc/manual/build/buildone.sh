afc57f2f92ec1bdc5eefb95db747fc62bceba32a
Frans Pop <fjp@debian.org>
2005-04-24 17:48:57 +0000

    Replace overlong if statement by case
    r27015

--- doc/manual/build/buildone.sh
+++ doc/manual/build/buildone.sh
@@ -277,12 +277,15 @@ RET=$?; [ $RET -ne 0 ] && exit 1
 BUILD_OK=""
 BUILD_FAIL=""
 for format in $formats ; do
-    if [ "$language" = "el" -o "$language" = "ja" -o "$language" = "ko" -o "$language" = "zh_CN" ] && \
-       [ "$format" = "pdf" -o "$format" = "ps" ] ; then
-        echo "Warning: pdf and ps formats are currently not supported for Chinese, Greek, Japanese and Korean"
-        BUILD_SKIP="$BUILD_SKIP $format"
-        continue
-    fi
+    case "$language" in
+        el|ja|ko|zh_CN)
+            if [ "$format" = "pdf" -o "$format" = "ps" ] ; then
+                echo "Warning: pdf and ps formats are currently not supported for Chinese, Greek, Japanese and Korean"
+                BUILD_SKIP="$BUILD_SKIP $format"
+                continue
+            fi
+            ;;
+    esac
 
     case $format in
         html)  create_html;;
@@ -312,7 +315,7 @@ for format in $formats ; do
 done
 
 # Clean up
-rm -r $tempdir
+#rm -r $tempdir
 
 # Evaluate the overall results
 [ -n "$BUILD_SKIP" ] && echo "Info: The following formats were skipped:$BUILD_SKIP"
