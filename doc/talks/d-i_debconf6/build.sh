b76e626584240fc102e3a8b49f151ff96746715f
Frans Pop <fjp@debian.org>
2006-04-08 03:56:34 +0000

    Add paper for D-I internals talk for debconf 6
    r36177

---
+++ doc/talks/d-i_debconf6/build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+xsltproc=`which xsltproc`
+lynx=`which lynx`
+sgmltools=`which sgmltools`
+w3m=`which w3m`
+stylesheet=/usr/share/sgml/docbook/stylesheet/xsl/nwalsh/html/chunk.xsl
+
+if [ -n "$xsltproc" ] ; then
+    if [ -e "$stylesheet" ]; then
+	$xsltproc style-html.xsl d-i_debconf6.xml
+    else
+	echo stylesheet missing. Please install the docbook-xsl Debian package
+	exit 1
+    fi
+else
+    echo xsltproc not found. Please install the xsltproc Debian package
+    exit 1
+fi
+
+if [ -f index.html ] ; then
+ # The mv thing breaks internal links
+ # mv index.html d-i_debconf6.html
+ if [ -n "$sgmltools" -a -n "$w3m" ] ; then
+    # To be checked
+    $sgmltools --backend=txt d-i_debconf6.html >d-i_debconf6.txt
+ else
+    if [ -n "$lynx" ] ; then
+	$lynx -dump -nolist d-i_debconf6.html >d-i_debconf6.txt
+    else
+	echo sgmltools, w3m or lynx not found. 
+        echo You need installing either sgmltools+w3m or lynx for
+        echo being able to convert the HTML documentation to text
+	exit 1
+    fi
+ fi
+fi
+
+
+
