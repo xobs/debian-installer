49d110bdc3b6b3122b976f704a04fc563103a51e
Wouter Verhelst <wouter@debian.org>
2009-04-20 11:52:49 +0000

    Remove this no longer used file
    r58270

--- build/config/powerpc/env.sh
+++
@@ -1,8 +0,0 @@
-# This script is sourced into the environment that daily-build is run in...
-
-TARGETS='build_powerpc_cdrom build_powerpc_netboot build_powerpc_netboot-gtk build_powerpc_hd-media build_powerpc64_netboot build_powerpc64_cdrom' 
-
-# this will overwrite $TARGETS right before the miboot build...
-TARGETS_MIBOOT='build_powerpc_floppy_root build_powerpc_floppy_net-drivers build_powerpc_floppy_cd-drivers build_powerpc_floppy_boot build_powerpc_floppy_boot-ofonly'
-
-export TARGETS TARGETS_MIBOOT
