2d27b2d53f84775fb1bd9b8a25892cefe750ec4a
Wouter Verhelst <wouter@debian.org>
2009-07-13 11:50:34 +0000

    Add support for Intel SS4000-E
    r59327

---
+++ build/util/arm/ss4000e/mkfwimage.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+INPUT=$1
+OUTPUT=$2
+
+if [ -z $OUTPUT -o -z $INPUT ]
+then
+	echo "Usage: $0 <inputfile> <outputfile>" >&2
+	exit 1
+fi
+
+echo -ne 'FALCONSTOR@EP219\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\08\0\0\0\0\0\0\09.9-b999\0\0\0\0\0\0\0\0fs-bc\0\0\0\0\0\0\0\0\0\0\0' > ${OUTPUT}.tmp
+cat $INPUT >> ${OUTPUT}.tmp
+MD5=$(md5sum ${OUTPUT}.tmp | awk -F' ' '{print $1}')
+(echo -ne $MD5; cat ${OUTPUT}.tmp) > $OUTPUT
+rm ${OUTPUT}.tmp
